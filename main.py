39850030cc2ce6c258e206b66fa5ba842e54f895
Hackrrr <Hackrrr@users.noreply.github.com>
2020-10-15 12:27:53 +0200

    Menší refractor 'main.py' V budoucnu se to snad předělá na lepší

--- main.py
+++ main.py
@@ -30,12 +30,12 @@ parser.add_argument(
     action="store_true",
     help="Interaktivní - Vyžaduje interakci po zobrazení zprávy/známky/..."
 )
-parser.add_argument(
-    "-t", "--test",
-    default=False,
-    action="store_true",
-    help="Test snippet"
-)
+# parser.add_argument(
+#     "-t", "--test",
+#     default=False,
+#     action="store_true",
+#     help="Test snippet"
+# )
 # parser.add_argument(
 #     "-d", "--debug",
 #     default=False,
@@ -48,11 +48,10 @@ url = args.url
 user = args.jmeno
 password = args.heslo
 interactive = args.interactive
-test = args.test
+
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
-
 def AnoNeDialog(text: str = "", help: bool = True) -> bool:
     while True:
         inputLetter = input(text + ("Ano/Ne: " if help else "")) # ano/yes/1 / ne/no/0
@@ -83,134 +82,147 @@ def DebugMode():
                 print(e)
     except KeyboardInterrupt:
         return
-def Timedelta2Tuple(delta: timedelta) -> tuple[int, int, int, int]: # days, hours, minutes, seconds
-    seconds = delta.seconds % 60 # TODO: Test
-    minutes = (delta.seconds - seconds) % 60
-    hours = (delta.seconds - (minutes * 60 + seconds)) % 24
-    days = delta.days
-    return (days, hours, minutes, seconds)
-
-
-try:
-    API = BakalariAPI.BakalariAPI(
-        BakalariAPI.Server(url),
-        user,
-        password,
-        False
-    )
-except BakalariAPI.Exceptions.InputException:
-    print("Neplatné URL schéma; Končím")
-    exit(1)
-
-
-print(f"Kontrola stavu serveru/webu... ({url})")
-
-if not API.server.Running():
-    print("Severver/web (pravděpodobně) neběží; Končím")
-    exit(1)
-
-print("Sever/web běží")
-
-print(f"Pokus o přihlášení jako '{user}'")
-
-try:
-    API.Login(False)
-except BakalariAPI.Exceptions.AuthenticationException:
-    print("Nepovedlo se přihlásit (nesprávné přihlašovací údaje)")
-    exit(1)
-
-print("Přihlášení úspěšné")
-#TODO: Init
-# Login done, now parse and extract
-
-
-
-if test:
-    
-    exit(0)
-
-
-
-
-
-print("Získávám IDčka zpráv...")
-zpravyIDs = API.GetKomensIDs()
-zpravy = []
-print("IDčka zpráv získany")
-for ID in zpravyIDs:
-    print(f"Získávám Komens zprávu {ID}")
-    zpravy.append(API.GetKomens(ID))
-print("Zprávy získány, zobrazuji...")
-if interactive:
+def Shell():
+    """Dočasné řešení interaktivity
+    Nespoléhat se na tohle - Bude se mazat nebo kompletně přepisovat na něco komplexnějšího...
+    """
     cls()
-for zprava in zpravy:
-    # try:
-    print("*** Zpráva ***")
-    print(zprava.Format())
-    print("\n\n\n")
+    print("Shell aktivní")
+    while True:
+        try:
+            print("BakalariAPI shell> ", end="")
+            inpt = input()
+            if len(inpt) == 0:
+                continue
+            if "help".startswith(inpt.lower()):
+                print("%-15s %s" % ("komens",   "Zobrazuje komens zprávy"))
+                print("%-15s %s" % ("schuzky",  "Zobrazuje online schůzky"))
+                print("%-15s %s" % ("studenti", "Zobrazuje studenty"))
+                print("%-15s %s" % ("znamky",   "Zobrazuje známky"))
+            elif "komens".startswith(inpt.lower()):
+                Komens()
+            elif "schuzky".startswith(inpt.lower()):
+                Schuzky()
+            elif "student".startswith(inpt.lower()):
+                Studenti()
+            elif "znamky".startswith(inpt.lower()):
+                Znamky()
+            else:
+                try:
+                    exec(f"print({inpt})")
+                except Exception as e:
+                    print(e)
+            
+        except KeyboardInterrupt:
+            exit(0)
+
+
+API = None
+
+def Login():
+    global API
+    try:
+        API = BakalariAPI.BakalariAPI(
+            BakalariAPI.Server(url),
+            user,
+            password,
+            False
+        )
+    except BakalariAPI.Exceptions.InputException:
+        print("Neplatné URL schéma; Končím")
+        exit(1)
+    print(f"Kontrola stavu serveru/webu... ({url})")
+    if not API.server.Running():
+        print("Severver/web (pravděpodobně) neběží; Končím")
+        exit(1)
+    print("Sever/web běží")
+    print(f"Pokus o přihlášení jako '{user}'")
+    try:
+        API.Login(False)
+    except BakalariAPI.Exceptions.AuthenticationException:
+        print("Nepovedlo se přihlásit (nesprávné přihlašovací údaje)")
+        exit(1)
+    print("Přihlášení úspěšné")
+def Komens():
+    print("Získávám IDčka zpráv...")
+    zpravyIDs = API.GetKomensIDs()
+    zpravy = []
+    print("IDčka zpráv získany")
+    for ID in zpravyIDs:
+        print(f"Získávám Komens zprávu {ID}")
+        zpravy.append(API.GetKomens(ID))
+    print("Zprávy získány, zobrazuji...")
     if interactive:
-        if zprava.NeedsConfirm and not zprava.Confirmed and AnoNeDialog("Zpráva vyžaduje potvrzení. Chcete potvrdit přečtení? "):
-            print("Potvrzuji zprávu...")
-            zprava.Confirm()
-            print("Zpráva potvrzena")
-        input("Pro pokračování stiskni klávasu...")
         cls()
-    # except KeyboardInterrupt:
-    #     print("Debug mód aktivní... Příkazy:")
-    #     DebugMode()
-
-print("Získávám známky...")
-znamky = API.GetGrades()
-print("Známky získány, zobrazuji...")
-if interactive:
-    cls()
-for znamka in znamky:
-    print("*** Známka ***")
-    print(znamka.Format())
-    print("\n")
+    for zprava in zpravy:
+        # try:
+        print("*** Zpráva ***")
+        print(zprava.Format())
+        print("\n\n\n")
+        if interactive:
+            if zprava.NeedsConfirm and not zprava.Confirmed and AnoNeDialog("Zpráva vyžaduje potvrzení. Chcete potvrdit přečtení? "):
+                print("Potvrzuji zprávu...")
+                zprava.Confirm()
+                print("Zpráva potvrzena")
+            input("Pro pokračování stiskni klávasu...")
+            cls()
+def Znamky():
+    print("Získávám známky...")
+    znamky = API.GetGrades()
+    print("Známky získány, zobrazuji...")
     if interactive:
-        input("Pro pokračování stiskni klávasu...")
         cls()
-
-schuzkyIDs = API.GetMettingsIDs()
-print("IDčka online schůzek získany")
-schuzky = []
-for ID in schuzkyIDs:
-    print(f"Získávám online chůzku {ID}")
-    schuzky.append(API.GetMeeting(ID))
-print("Zprávy získány, zobrazuji...")
-if interactive:
-    cls()
-for schuzka in schuzky:
-    print("*** Online Schůzka ***")
-    print(schuzka.Format())
-    print("\n\n")
+    for znamka in znamky:
+        print("*** Známka ***")
+        print(znamka.Format())
+        print("\n")
+        if interactive:
+            input("Pro pokračování stiskni klávasu...")
+            cls()
+def Schuzky():
+    schuzkyIDs = API.GetMettingsIDs()
+    print("IDčka online schůzek získany")
+    schuzky = []
+    for ID in schuzkyIDs:
+        print(f"Získávám online chůzku {ID}")
+        schuzky.append(API.GetMeeting(ID))
+    print("Zprávy získány, zobrazuji...")
     if interactive:
-        diff = schuzka.StartTime - datetime.now(timezone.utc).astimezone()
-        if diff <= timedelta(minutes=30) and AnoNeDialog(f"Chcete otevřít online schůzku? Shůzka začíná do 30ti minut... "):
-            print("Otevírám...")
-            webbrowser.open_new_tab(schuzka.JoinURL)
-        input("Pro pokračování stiskni klávasu...")
         cls()
-
-if interactive and AnoNeDialog("Při získávání schůzek se podařilo získat seznam všech studentů. Chcete jej zobrazit? "):
-    count = InputCislo("Kolik výsledků najednou? (Výchozí 25) ", 25)
-    offset = 0
-    length = len(API.Students)
-    cls()
-    while offset < length:
-        for x in range(count):
-            if (offset >= length):
-                break
-            print(API.Students[offset].Format())
-            offset += 1
-        input(f"Pro pokračování stiskni klávasu... (Již zobrazeno {offset} výsledků z {length})")
+    for schuzka in schuzky:
+        print("*** Online Schůzka ***")
+        print(schuzka.Format())
+        print("\n\n")
+        if interactive:
+            diff = schuzka.StartTime - datetime.now(timezone.utc).astimezone()
+            if diff <= timedelta(minutes=30) and AnoNeDialog(f"Chcete otevřít online schůzku? Shůzka začíná do 30ti minut... "):
+                print("Otevírám...")
+                webbrowser.open_new_tab(schuzka.JoinURL)
+            input("Pro pokračování stiskni klávasu...")
+            cls()
+def Studenti():
+    if interactive and len(API.Students) and AnoNeDialog("Podařilo získat seznam studentů. Chcete jej zobrazit? "):
+        count = InputCislo("Kolik výsledků najednou? (Výchozí 25) ", 25)
+        offset = 0
+        length = len(API.Students)
         cls()
+        while offset < length:
+            for _ in range(count):
+                if (offset >= length):
+                    break
+                print(API.Students[offset].Format())
+                offset += 1
+            input(f"Pro pokračování stiskni klávasu... (Již zobrazeno {offset} výsledků z {length})")
+            cls()
+def Konec():
+    API.Logout()
 
+Login()
+if interactive:
+    Shell()
 
-
-
-
-
-
-API.Logout()
+Komens()
+Znamky()
+Schuzky()
+Studenti()
+Konec()
